2588f7d26a8ebe9271a45997f0af99c661595923
zhongnuo <zhongnuo@realtek-sg.com>
2025-10-23 09:55:05 +0800

    build/configs/rtl8721f: add prebuild binaries, build scripts

---
+++ build/configs/rtl8721f/rtl8721f_download.sh
@@ -0,0 +1,249 @@
+#!/usr/bin/env bash
+###########################################################################
+#
+# Copyright 2025 Samsung Electronics All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+#
+###########################################################################
+# rtl8730e_download.sh
+
+TOOL_PATH=${TOP_PATH}/build/tools/amebagreen2
+IMG_TOOL_PATH=${TOOL_PATH}/image_tool
+
+OS_PATH=${TOP_PATH}/os
+CONFIG=${OS_PATH}/.config
+source ${CONFIG}
+APP_NUM=0
+LAST_IMAGE=0
+LAST_OFFSET=0
+USB_DOWNLOAD=0
+HAS_RESOURCE=0
+
+VID_FOUND=0
+RETRY_COUNT=0
+MAX_RETRIES=10
+function pre_download()
+{
+	while [ $VID_FOUND -ne 1 ] && [ $RETRY_COUNT -lt $MAX_RETRIES ]; do
+		VENDOR_ID=$(udevadm info -a -n "$TTYDEV" | grep -m1 'ATTRS{idVendor}' | sed -nE 's/.*"([0-9a-fA-F]+)".*/\1/p')
+
+		if [ -n "$VENDOR_ID" ]; then
+			VID_FOUND=1
+			echo "Found VID: $VENDOR_ID"
+			break
+		else
+			echo "VID not found, retrying..."
+			((RETRY_COUNT++))
+			sleep 1
+		fi
+	done
+	if [ $VID_FOUND -ne 1 ]; then
+		echo "Port name $TTYDEV not valid"
+		exit 1
+	fi
+
+	if [[ $VENDOR_ID == "0bda" ]]; then
+		USB_DOWNLOAD=1
+	else
+		USB_DOWNLOAD=0
+	fi
+
+	source ${TOP_PATH}/os/.bininfo
+	if [ -f ${IMG_TOOL_PATH}/USB_download_setting.txt ]; then
+		rm -rf ${IMG_TOOL_PATH}/USB_download_setting.txt
+	fi
+	if [ -f ${IMG_TOOL_PATH}/USB_erase_setting.txt ]; then
+		rm -rf ${IMG_TOOL_PATH}/USB_erase_setting.txt
+	fi
+	cp -p ${BIN_PATH}/${BL1}.bin ${IMG_TOOL_PATH}/${BL1}.bin 
+	cp -p ${BIN_PATH}/${KERNEL_BIN_NAME} ${IMG_TOOL_PATH}/${KERNEL_BIN_NAME}
+	if [ "${CONFIG_APP_BINARY_SEPARATION}" == "y" ]; then
+		if test -f "${BIN_PATH}/${APP1_BIN_NAME}"; then
+			APP_NUM=$(($APP_NUM+1))
+			cp -p ${BIN_PATH}/${APP1_BIN_NAME} ${IMG_TOOL_PATH}/${APP1_BIN_NAME}
+		fi
+		if test -f "${BIN_PATH}/${APP2_BIN_NAME}"; then
+			APP_NUM=$(($APP_NUM+1))
+			cp -p ${BIN_PATH}/${APP2_BIN_NAME} ${IMG_TOOL_PATH}/${APP2_BIN_NAME}
+		fi
+		if [ ${APP_NUM} -eq 0 ]; then
+			echo "No User Binary."
+			post_download
+			exit 1
+		fi
+	fi
+	if test -f "${BIN_PATH}/${COMMON_BIN_NAME}"; then
+		cp -p ${BIN_PATH}/${COMMON_BIN_NAME} ${IMG_TOOL_PATH}/${COMMON_BIN_NAME}
+	fi
+	if test -f "${BIN_PATH}/${RESOURCE_BIN_NAME}"; then
+		HAS_RESOURCE=1
+		cp -p ${BIN_PATH}/${RESOURCE_BIN_NAME} ${IMG_TOOL_PATH}/${RESOURCE_BIN_NAME}
+	fi
+	if test -f "${SMARTFS_BIN_PATH}"; then
+		cp -p ${SMARTFS_BIN_PATH} ${IMG_TOOL_PATH}/${CONFIG_ARCH_BOARD}_smartfs.bin
+	fi
+	if test -f "${BIN_PATH}/${BOOTPARAM}.bin"; then
+		cp -p ${BIN_PATH}/${BOOTPARAM}.bin ${IMG_TOOL_PATH}/${BOOTPARAM}.bin
+	fi
+	if test -f "${BIN_PATH}/${EXTERNAL}.bin"; then
+		cp -p ${BIN_PATH}/${EXTERNAL}.bin ${IMG_TOOL_PATH}/${EXTERNAL}.bin
+	fi
+
+	for partidx in ${!parts[@]}; do
+		if [[ "${parts[$partidx]}" == "reserved" ]];then
+			continue
+		fi
+
+		if [[ "${parts[$partidx]}" == "ftl" ]];then
+			continue
+		fi
+
+		if [[ "${parts[$partidx]}" == "ss" ]];then
+			continue
+		fi
+		LAST_OFFSET=${offsets[$partidx]}
+	done
+	LAST_IMAGE=${BOOTPARAM}.bin
+
+	if [[ -n ${CONFIG_ARCH_BOARD_HAVE_SECOND_FLASH} ]];then
+		source ${TOP_PATH}/build/configs/${CONFIG_ARCH_BOARD}/board_metadata.txt
+		if test -f "${BIN_PATH}/${EXTERNAL}.bin"; then
+			cp -p ${BIN_PATH}/${EXTERNAL}.bin ${IMG_TOOL_PATH}/${EXTERNAL}.bin
+		fi
+
+		if [ "$HAS_RESOURCE" -eq "1" ]; then
+			LAST_IMAGE=${RESOURCE_BIN_NAME}
+		fi
+	fi
+	if [ "$USB_DOWNLOAD" -eq "1" ]; then
+		touch "${IMG_TOOL_PATH}/USB_download_setting.txt"
+		touch "${IMG_TOOL_PATH}/USB_erase_setting.txt"
+	fi
+}
+
+function board_download()
+{
+	cd ${IMG_TOOL_PATH}
+	if [ ! -f ${IMG_TOOL_PATH}/$3 ];then
+		echo "$3 not present"
+	else
+		if [ "$USB_DOWNLOAD" -eq "0" ]; then
+			#echo "UART download"
+			./upload_image_tool_linux "download" $1 1 $2 $3
+		fi
+		if [ "$USB_DOWNLOAD" -eq "1" ]; then
+			#echo "USB download"
+			#echo "Save info to USB_download_setting.txt"
+			echo "$3" >> USB_download_setting.txt
+			echo "$2" >> USB_download_setting.txt
+			if [ "$6" == "ALL" ] && [ "$3" == "$LAST_IMAGE" ]; then
+				echo ""
+				echo "==================================="
+				echo "Start USB full download in Flash"
+				echo "==================================="
+				./upload_image_tool_linux "download" $1 "ALL"
+				echo "Complete USB download"
+			elif [ "$6" == "KERNEL" ]; then
+				return;
+			elif [ "$6" == "bootparam" ]; then
+				echo ""
+				echo "========================================="
+				echo "Start USB download to kernel partition"
+				echo "========================================="
+				./upload_image_tool_linux "download" $1 "ALL"
+				echo "Complete USB download"
+			elif [ "$6" != "ALL" ]; then
+				echo ""
+				echo "========================================="
+				echo "Start USB download to $5 partition"
+				echo "========================================="
+				./upload_image_tool_linux "download" $1 $2 $3
+				echo "Complete USB download"
+			else
+				return;
+			fi
+		fi
+	fi
+}
+
+function board_erase()
+{
+	cd ${IMG_TOOL_PATH}
+	if [ "$USB_DOWNLOAD" -eq "0" ]; then
+		#echo "UART erase"
+		./upload_image_tool_linux "erase" $1 1 $2 $3
+	fi
+	if [ "$USB_DOWNLOAD" -eq "1" ]; then
+		#echo "USB erase"
+		#echo "Save info to USB_erase_setting.txt"
+		echo "$2" >> USB_erase_setting.txt
+		echo "$3" >> USB_erase_setting.txt
+		if [ "$5" == "ALL" ] && [ "$2" == "$LAST_OFFSET" ]; then
+			echo ""
+			echo "==================================="
+			echo "Start USB full erase in Flash"
+			echo "==================================="
+			./upload_image_tool_linux "erase" $1 "ALL"
+			echo "Complete USB erase"
+		elif [ "$5" != "ALL" ]; then
+			echo ""
+			echo "==================================="
+			echo "Start USB $4 erase in Flash"
+			echo "==================================="
+			./upload_image_tool_linux "erase" $1 $2 $3
+			echo "Complete USB download"
+		else
+			return;
+		fi
+	fi
+}
+
+function post_download()
+{
+	cd ${IMG_TOOL_PATH}
+	[ -e ${BL1}.bin ] && rm ${BL1}.bin
+	if test -f "${KERNEL_BIN_NAME}"; then
+		[ -e ${KERNEL_BIN_NAME} ] && rm ${KERNEL_BIN_NAME}
+	fi
+	if test -f "${APP1_BIN_NAME}"; then
+		[ -e ${APP1_BIN_NAME} ] && rm ${APP1_BIN_NAME}
+	fi
+	if test -f "${APP2_BIN_NAME}"; then
+		[ -e ${APP2_BIN_NAME} ] && rm ${APP2_BIN_NAME}
+	fi
+	if test -f "${COMMON_BIN_NAME}"; then
+		[ -e ${COMMON_BIN_NAME} ] && rm ${COMMON_BIN_NAME}
+	fi
+	if test -f "${SMARTFS_BIN_PATH}"; then
+		[ -e ${CONFIG_ARCH_BOARD}_smartfs.bin ] && rm ${CONFIG_ARCH_BOARD}_smartfs.bin
+	fi
+	if test -f "${RESOURCE_BIN_PATH}"; then
+		[ -e  ] && rm ${RESOURCE_BIN_PATH}
+	fi
+	if test -f "${BOOTPARAM}.bin"; then
+		[ -e ${BOOTPARAM}.bin ] && rm ${BOOTPARAM}.bin
+	fi
+	if test -f "${RESOURCE_BIN_NAME}"; then
+		[ -e ${RESOURCE_BIN_NAME} ] && rm ${RESOURCE_BIN_NAME}
+	fi
+	if test -f "${EXTERNAL}.bin"; then
+		[ -e ${EXTERNAL}.bin ] && rm ${EXTERNAL}.bin
+	fi
+	if [ "$USB_DOWNLOAD" -eq "1" ]; then
+		rm "${IMG_TOOL_PATH}/USB_download_setting.txt"
+		rm "${IMG_TOOL_PATH}/USB_erase_setting.txt"
+	fi
+}
+
+
---
+++ build/configs/rtl8721f/rtl8721f_make_bin.sh
@@ -0,0 +1,229 @@
+#!/usr/bin/env bash
+###########################################################################
+#
+# Copyright 2025 Samsung Electronics All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+#
+###########################################################################
+THIS_PATH=`test -d ${0%/*} && cd ${0%/*}; pwd`
+TOP_PATH=${THIS_PATH}/../../..
+CONFIG=${TOP_PATH}/os/.config
+BUILDDIR=${TOP_PATH}/build
+BINDIR=${BUILDDIR}/output/bin
+GNUUTL=${BUILDDIR}/tools/amebagreen2/gnu_utility
+TOOL_PATH=${THIS_PATH}/../../tools/amebagreen2
+BOOT_PATH=${TOOL_PATH}/bootloader
+FLOADER_PATH=${TOOL_PATH}/flashloader
+
+echo "Realtek Postbuild Actions"
+export TOPDIR=$BUILDDIR/
+
+cp $BINDIR/tinyara.axf $BINDIR/target_img2.axf
+
+arm-none-eabi-nm $BINDIR/target_img2.axf | sort > $BINDIR/target_img2.map
+
+arm-none-eabi-objdump -d $BINDIR/target_img2.axf > $BINDIR/target_img2.asm
+
+#arm-none-eabi-objdump -h -S --disassemble-all $BINDIR/target_img2.axf > $BINDIR/target_img2.txt
+
+cp $BINDIR/target_img2.axf $BINDIR/target_pure_img2.axf
+
+arm-none-eabi-strip $BINDIR/target_pure_img2.axf
+
+arm-none-eabi-objcopy -j .psram_image2.text.data -j .ARM.extab -j .ARM.exidx -Obinary $BINDIR/target_pure_img2.axf $BINDIR/psram_2.bin
+arm-none-eabi-objcopy -j .sram_image2.text.data -Obinary $BINDIR/target_pure_img2.axf $BINDIR/sram_2.bin
+arm-none-eabi-objcopy -j .xip_image2.text -Obinary $BINDIR/target_pure_img2.axf $BINDIR/xip_image2.bin
+
+
+
+#For Bluetooth Trace
+if [ "${CONFIG_BT_EN}" == "y" ];then
+	arm-none-eabi-objcopy -j .bluetooth_trace.text \
+	-Obinary $BINDIR/target_pure_img2.axf $BINDIR/APP.trace
+fi
+
+echo "========== Image Info HEX =========="
+arm-none-eabi-size -A --radix=16 $BINDIR/target_img2.axf
+arm-none-eabi-size -t --radix=16 $BINDIR/target_img2.axf
+
+echo "========== Image Info DEC =========="
+arm-none-eabi-size -A --radix=10 $BINDIR/target_img2.axf
+arm-none-eabi-size -t --radix=10 $BINDIR/target_img2.axf
+
+echo "========== Image manipulating start =========="
+
+# Pad binaries (32 bytes alignment)
+$GNUUTL/pad_align.sh $BINDIR/psram_2.bin 32
+$GNUUTL/pad_align.sh $BINDIR/sram_2.bin 32
+$GNUUTL/pad_align.sh $BINDIR/xip_image2.bin 32
+
+# Prepend headers
+$GNUUTL/prepend_header.sh $BINDIR/sram_2.bin __sram_image2_start__ $BINDIR/target_img2.map
+$GNUUTL/prepend_header.sh $BINDIR/psram_2.bin __psram_image2_start__ $BINDIR/target_img2.map
+$GNUUTL/prepend_header.sh $BINDIR/xip_image2.bin __flash_text_start__ $BINDIR/target_img2.map
+
+
+cat $BINDIR/xip_image2_prepend.bin $BINDIR/sram_2_prepend.bin $BINDIR/psram_2_prepend.bin > $BINDIR/km4tz_image2_all.bin
+
+#python $GNUUTL/axf2bin.py imagetool $BINDIR/km4tz_image2_all.bin
+
+
+
+function copy_bootloader()
+{
+	if [ ! -f ${CONFIG} ];then
+		echo "No .config file"
+		exit 1
+	fi
+
+	source ${CONFIG}
+
+	echo "========== Copy_bootloader =========="
+	if [ ! -f $BOOT_PATH/amebagreen2_boot.bin ];then
+		echo "No amebagreen2_boot.bin"
+		exit 1
+	fi
+	cp $BOOT_PATH/amebagreen2_boot.bin $BINDIR/amebagreen2_boot.bin
+
+}
+
+function concatenate_binary_without_signing()
+{
+	if [ ! -f ${CONFIG} ];then
+		echo "No .config file"
+		exit 1
+	fi
+
+	source ${CONFIG}
+
+ 	echo "========== Concatenate_binary =========="
+
+	if [ ! -f $BINDIR/km4ns_image2_all.bin ] || [ ! -f $BINDIR/km4tz_image2_all.bin ];then
+		echo "No km4ns_image2_all.bin or km4tz_image2_all.bin"
+		exit 1
+	fi
+ 	#cp $BINDIR/km4ns_image2_all.bin $GNUUTL/km4ns_image2_all_temp.bin
+ 	# $GNUUTL/rmcert.sh $GNUUTL/km4ns_image2_all_temp.bin
+ 	# $GNUUTL/pad.sh $GNUUTL/km4ns_image2_all_temp.bin
+ 	# cat $GNUUTL/cert.bin $GNUUTL/km4ns_image2_all_temp.bin $BINDIR/km4tz_image2_all.bin > $BINDIR/amebagreen2_app.bin
+ 	cat $GNUUTL/app_cert.bin $GNUUTL/manifest_app.bin $GNUUTL/km4ns_image2_all.bin $BINDIR/km4tz_image2_all.bin $BINDIR/km4tz_image3_all.bin > $BINDIR/amebagreen2_app.bin
+ 	#rm -rf $GNUUTL/km4ns_image2_all_temp.bin
+
+}
+
+function concatenate_binary_with_signing()
+{
+	if [ ! -f ${CONFIG} ];then
+		echo "No .config file"
+		exit 1
+	fi
+
+	source ${CONFIG}
+
+	#signing
+	echo "========== Binary SIGNING =========="
+	bash $BUILDDIR/configs/rtl8721f/rtl8721f_signing.sh kernel
+}
+
+#*****************************************************************************#
+#              COPY flashloader into bin output folder                        #
+#*****************************************************************************#
+function copy_flashloader()
+{
+	if [ ! -f ${CONFIG} ];then
+		echo "No .config file"
+		exit 1
+	fi
+
+	source ${CONFIG}
+
+	echo "========== Copy flashloader into bin output folder=========="
+	cp $FLOADER_PATH/flash_loader_ram_1.bin $BINDIR/flash_loader_ram_1.bin
+	cp $FLOADER_PATH/target_FPGA.axf $BINDIR/target_FPGA.axf
+}
+
+#*****************************************************************************#
+#              COPY km4ns_image into bin output folder                       #
+#*****************************************************************************#
+function copy_km4_image3()
+{
+	if [ ! -f ${CONFIG} ];then
+		echo "No .config file"
+		exit 1
+	fi
+
+	source ${CONFIG}
+	
+	echo "========== Copy km4tz_image3_all into bin output folder=========="
+	if [ ! -f $GNUUTL/km4tz_image3_all.bin ];then
+		echo "No km4tz_image3_all.bin"
+		exit 1
+	fi
+	
+	if [ "${CONFIG_AMEBAGREEN2_TRUSTZONE}" == "y" ];then
+		cp $GNUUTL/km4tz_image3_all.bin $BINDIR/km4tz_image3_all.bin
+	fi
+}
+function copy_km4ns_image()
+{
+	if [ ! -f ${CONFIG} ];then
+		echo "No .config file"
+		exit 1
+	fi
+
+	source ${CONFIG}
+
+	echo "========== Copy km4ns_image2_all into bin output folder=========="
+	if [ ! -f $GNUUTL/km4ns_image2_all.bin ];then
+		echo "No km4ns_image2_all.bin"
+		exit 1
+	fi
+	cp $GNUUTL/km4ns_image2_all.bin $BINDIR/km4ns_image2_all.bin
+}
+
+#*****************************************************************************#
+#              Temporary: Remove the Large Size Binary                        #
+#*****************************************************************************#
+function remove_large_binary_temp()
+{
+	if [ ! -f ${CONFIG} ];then
+		echo "No .config file"
+		exit 1
+	fi
+
+	source ${CONFIG}
+
+	echo "========== Remove large size binary, temporary fix=========="
+	if [ ! -f $BINDIR/tinyara.axf.bin ];then
+		echo "No tinyara.axf.bin"
+		exit 1
+	fi
+	rm $BINDIR/tinyara.axf.bin
+}
+
+
+copy_bootloader;
+copy_flashloader;
+copy_km4ns_image;
+copy_km4_image3;
+if [ "${CONFIG_BINARY_SIGNING}" == "y" ];then
+	concatenate_binary_with_signing;
+
+	# Binary Signing is not support in public, so copy the non-signing binary
+	concatenate_binary_without_signing;
+else
+	concatenate_binary_without_signing;
+fi
+remove_large_binary_temp;
+ 
