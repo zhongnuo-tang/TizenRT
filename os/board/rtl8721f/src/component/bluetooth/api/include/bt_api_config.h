b52c90e5f1d12ada9dc4d408d36a08ec3b206b15
ZivLow <zivlowenhao@gmail.com>
2025-12-05 11:15:58 +0800

    os/board/rtl8721f: TizenRT customization for bt api/include/bt_api_config.h
    - enable bt coc

--- os/board/rtl8721f/src/component/bluetooth/api/include/bt_api_config.h
+++ os/board/rtl8721f/src/component/bluetooth/api/include/bt_api_config.h
@@ -168,7 +168,11 @@ extern "C"
 #define RTK_BLE_5_1_SUPPORT                 0
 #define RTK_BLE_5_2_SUPPORT                 0
 #define RTK_BLE_SMP_OOB_SUPPORT             1
+#ifdef CONFIG_PLATFORM_TIZENRT_OS
+#define RTK_BLE_COC_SUPPORT                 1
+#else
 #define RTK_BLE_COC_SUPPORT                 0
+#endif //#ifdef CONFIG_PLATFORM_TIZENRT_OS
 #endif /* RTK_BLE_SUPPORT */
 
 #if defined(RTK_BLE_4_2_SUPPORT) && RTK_BLE_4_2_SUPPORT
